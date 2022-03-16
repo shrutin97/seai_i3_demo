c57a50cd1265f78fa4fe1c174646ed4fc9807afa
mukundadas <mukundad@andrew.cmu.edu>
2022-03-16 00:02:52 -0400

    failed recs

--- operations.py
+++ operations.py
@@ -17,4 +17,4 @@ def matrix_multiply(a, b):
 def recommend(userid):
     recs = ['twister+1996', 'the+wolf+of+wall+street+2013', 'the+twilight+saga+new+moon+2009', 'the+patriot+2000', 'the+crazies+2010', 'the+core+2003', 'stranger+than+fiction+2006', 'raiders+of+the+lost+ark+1981', 'north+by+northwest+1959',
             'in ++out+1997', 'cloverfield+2008', 'broken+arrow+1996', 'american+pie+1999', 'white+christmas+1954', 'vicky+cristina+barcelona+2008', 'thunderball+1965', 'the+usual+suspects+1995', 'the+town+2010', 'the+terminal+2004', 'the+rock+1996']
-    return recs
+    return recs[:10]
