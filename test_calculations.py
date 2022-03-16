716ed9ab05e2c84bbb993877b53832064fb98d97
mukundadas <mukundad@andrew.cmu.edu>
2022-03-16 00:53:01 -0400

    fixes to test cases

--- test_calculations.py
+++ test_calculations.py
@@ -24,7 +24,7 @@ def test_matrix_multiply_res():
     assert (res == matrix_multiply(a, b)).all()
 
 
-def test_matrix_multiply_res():
+def test_matrix_multiply_shape():
     a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     b = np.array([[10, 20, 30], [40, 50, 60], [1, 2, 3]])
     assert a.shape[1] == b.shape[0]
